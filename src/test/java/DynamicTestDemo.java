a2416d0257f90840397d704d520178ffacb8fbb1
Ryosuke Uchitate <ry0suke.uch1tate@gmail.com>
2018-01-16 01:03:45 +0900

    implement Standard Dynamic Test

--- src/test/java/DynamicTestDemo.java
+++ src/test/java/DynamicTestDemo.java
@@ -4,12 +4,34 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.TestFactory;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
 import java.util.stream.Stream;
 
 class DynamicTestDemo {
 
 	@TestFactory
-	Stream<DynamicTest> personDynamicTest() {
+	Collection<DynamicTest> personDynamicTestCollection() {
+		List<Person> people = Arrays.asList(new Person("Ryosuke", "Uchitate", 27, Gender.MAN), new Person("Hanako", "Yamada", 15, Gender.WOMAN));
+		return Arrays.asList(
+				DynamicTest.dynamicTest("1st person test", () -> Assertions.assertEquals("Ryosuke Uchitate", people.get(0).getFullName())),
+				DynamicTest.dynamicTest("1st person test", () -> Assertions.assertEquals("Hanako Yamada", people.get(1).getFullName()))
+		);
+	}
+
+	@TestFactory
+	Iterator<DynamicTest> personDynamicTestIterator() {
+		List<Person> people = Arrays.asList(new Person("Ryosuke", "Uchitate", 27, Gender.MAN), new Person("Hanako", "Yamada", 15, Gender.WOMAN));
+		return Arrays.asList(
+				DynamicTest.dynamicTest("1st person test", () -> Assertions.assertEquals("Ryosuke Uchitate", people.get(0).getFullName())),
+				DynamicTest.dynamicTest("1st person test", () -> Assertions.assertEquals("Hanako Yamada", people.get(1).getFullName()))
+		).iterator();
+	}
+
+	@TestFactory
+	Stream<DynamicTest> personDynamicTestStream() {
 		return Stream.of(
 				new Person("Ryosuke", "Uchitate", 27, Gender.MAN),
 				new Person("Hanako", "Yamada", 15, Gender.WOMAN)
